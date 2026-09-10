daa72e70ac452458a2d3aeecee3595c6a7dbefcd
Zankar Sakharkar <zankar.sakharkar@gmail.com>
2026-09-10 13:16:23 +0530

    Switch main.jsx to use split index.css instead of global.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import "./styles/variables.css";
-import "./styles/global.css";
-import "./styles/responsive.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
